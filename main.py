2ab0b0391b5d8a02a8aa1fbeb4a4da4c89c52e76
Gregg Stelmach <gstelmach@godaddy.com>
2023-11-27 22:06:38 -0700

    second

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     with open("transaction_v2_schema.json", "r") as read_file:
         schema = json.load(read_file)
-    with open("transaction_v2_messsage.json", "r") as read_message_file:
+    with open("transaction_v2_message.json", "r") as read_message_file:
         message = json.load(read_message_file)
 
     validator = fastjsonschema.compile(schema)
